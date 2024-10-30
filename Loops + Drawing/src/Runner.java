25fdbeca994f91b8ceeda9b1f11020fc0aa601aa
Arnav-Jadi <114833732+Arnav-Jadi@users.noreply.github.com>
2024-10-30 10:02:32 -0700

    Update Runner.java
    updated with finished code

--- Loops + Drawing/src/Runner.java
+++ Loops + Drawing/src/Runner.java
@@ -14,50 +14,30 @@ public class Runner extends JPanel{
 	public void paint(Graphics g) {
 		
 		Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(5));
-        
+        g2.setStroke(new BasicStroke(2));
+   
+		
+		 int var = 0;
+		 for (; var < 500; var+=20) {
+			 
+			 g2.drawLine(20, 545-var, 20+var, 10);
+			 g2.drawLine(20, 10+var, 20+var, 545);
+		 }
 		 
-		//1) draw a concentric rectangle that's 10pixels from all sides of the "gray"
-		//portion of the GUI
-		g2.drawRect(10, 10, 765, 540);
-		
-		
-		//2) Draw a horizontal line to bisect the Rectangle
-		g2.drawLine(10, 270, 770, 270);
-		
-		
-		//3) Draw a vertical line to bisect the shape again
-		g2.drawLine(765/2, 10, 765/2,550 );
-		
-		
-		//4) small rectangles on top-left
-		g2.drawLine(765/4, 10, 765/4, 550/2-10);
-		
-		
-		/* 
-		 * int var = 0; 						<---- control variable
-		 * while(var < 10){ 					<---- condition to run the loop
-		 *   
-		 *   //any repeating code
-		 *   
-		 *   var++;								<---- update control variable
-		 *   
-		 * }
-		 */
-		
+		 var = 0;
 		 
-
-	
+		 while (var < 500) {
+			 
+			 g2.drawLine(765, 545-var, 765-var, 10);
+			 g2.drawLine(765, 20+var, 765-var, 545);
+			 var+=20;
+		 }
+			
 	
 	}
 	
 	
-	
-	
-	
-	
-	
-	
+
 	
 	public static void main(String[] arg) {
 		Runner m = new Runner();
